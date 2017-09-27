17d155e9c63555d1d328001d3c048a4f7b72d946
Morgan Nimmons <morgannimmons@gmail.com>
2017-09-27 20:19:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function makeInt(n){
   return parseInt('n', 10);
 }
 
-function preserveDecimal(){
-  return parseFloat('sldkjflksjf', 10);
+function preserveDecimal(n){
+  return parseFloat('n');
+  return parseFloat('n', 10);
 }
